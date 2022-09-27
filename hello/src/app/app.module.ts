c96432e57ba0ca6e2ac2305fa7bd9c7e0cec8fdc
ricardodo10 <ricardocoutinhosilva1999@gmail.com>
2022-09-27 16:09:02 +0200

    first commity

--- hello/src/app/app.module.ts
+++ hello/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodosComponent } from './components/todos/todos.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent,
+  
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
